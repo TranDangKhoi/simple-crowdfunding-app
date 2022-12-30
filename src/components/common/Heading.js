5a685e8b1a7912ea5f704e7d47f4e4d7618e79d4
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-12-30 16:39:20 +0700

    :100: Create a heading component for re-use

---
+++ src/components/common/Heading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Heading = ({ campaignAmount = null, children, className = "" }) => {
+  return (
+    <h2 className={`${className} text-lg font-semibold text-text1`}>
+      {children}
+      {campaignAmount && (
+        <span className="text-secondary"> {`(${campaignAmount})`}</span>
+      )}
+    </h2>
+  );
+};
+
+export default Heading;
