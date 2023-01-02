8544b76a2a01e418fca987ba833314b02edad4a2
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2023-01-02 23:23:12 +0700

    :wink: Add arrow up icon

---
+++ src/components/icon/ArrowUpIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ArrowUpIcon = () => {
+  return (
+    <svg
+      className="rotate-180"
+      width={12}
+      height={8}
+      viewBox="0 0 12 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 1L6 6L11 1" stroke="currentColor" strokeWidth={2} />
+    </svg>
+  );
+};
+
+export default ArrowUpIcon;
