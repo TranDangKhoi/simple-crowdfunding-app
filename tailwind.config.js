0a35549497b905c5b6f1e511e2d1c0084ac80ff8
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2023-01-06 23:58:15 +0700

    :heavy_plus_sign: Add new box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,8 @@ module.exports = {
         darkRed: "#422C32",
       },
       boxShadow: {
+        tabShadow:
+          "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
         itemShadow:
           "0px 2px 4px rgba(184, 184, 184, 0.1), 0px 6px 12px rgba(184, 184, 184, 0.02), 0px 12px 20px rgba(184, 184, 184, 0.03)",
         softShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
